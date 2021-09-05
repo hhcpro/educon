1a1b6e97422a7dcb9018ac28283bc5fcc88381e2
teoleg <oleg_terletsky@hotmail.com>
2021-09-05 15:06:20 -0400

    Update table css

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -3,9 +3,27 @@
         <form @submit.prevent="videos">
             <button>Get List of Classes</button>
             <h1>Catalog</h1>
-            <div v-for="video in myVideos" :key="video.id">
-                <h3>{{ video.filename }}</h3>
-            </div>
+            
+            <!--div v-for="video in myVideos" :key="video.id">
+                <h3>{{ video.filename }}</h3>  
+            </div-->
+            <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope='col'>ID</th>
+                            <th scope='col'>Name (Alias)</th>
+                            <th scope='col'>Creation Date</th>
+                        </tr>
+
+                    </thead>
+                    <tbody>
+                        <tr v-for="(column, index) in myVideos" :key="column.id">   
+                            <td>{{ index }} </td>
+                            <td>{{ column.filename }}</td>
+                            <td>{{ column.createdAt }}</td>
+                        </tr>
+                    </tbody>
+                </table>
         </form>
     </div>
 </template>
@@ -55,5 +73,38 @@ form {
     margin-bottom: 20px;
     color: brown;
     background-image: radial-gradient();
+}
+table {
+  font-family: 'Open Sans', sans-serif;
+  width: 750px;
+  border-collapse: collapse;
+  border: 3px solid #44475C;
+  margin: 10px 10px 0 10px;
+}
+
+table th {
+  text-transform: uppercase;
+  text-align: left;
+  background: #44475C;
+  color: #FFF;
+  padding: 8px;
+  min-width: 30px;
+}
+
+table td {
+  text-align: left;
+  padding: 8px;
+  border-right: 2px solid #7D82A8;
+}
+table td:last-child {
+  border-right: none;
+}
+table tbody tr:nth-child(2n) td {
+  background: #D4D8F9;
+}
+
+button {
+    color: darkblue;
+    text-emphasis-color: red;
 }
 </style>
