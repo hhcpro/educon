351676cd9e629d6c3d91b280d4aca32c24b93edd
Oleg Terletsky <oleg_terletsky@hotmail.com>
2021-09-12 01:36:44 -0400

    Restructure backend

--- src/models/schema.js
+++ src/models/schema.js
@@ -1,7 +1,7 @@
 export const schema = {
     "models": {
-        "Note": {
-            "name": "Note",
+        "Notes": {
+            "name": "Notes",
             "fields": {
                 "id": {
                     "name": "id",
@@ -10,30 +10,10 @@ export const schema = {
                     "isRequired": true,
                     "attributes": []
                 },
-                "message": {
-                    "name": "message",
+                "content": {
+                    "name": "content",
                     "isArray": false,
                     "type": "String",
-                    "isRequired": false,
-                    "attributes": []
-                },
-                "Videos": {
-                    "name": "Videos",
-                    "isArray": false,
-                    "type": {
-                        "model": "Videos"
-                    },
-                    "isRequired": false,
-                    "attributes": [],
-                    "association": {
-                        "connectionType": "BELONGS_TO",
-                        "targetName": "noteVideosId"
-                    }
-                },
-                "videosID": {
-                    "name": "videosID",
-                    "isArray": false,
-                    "type": "ID",
                     "isRequired": false,
                     "attributes": []
                 },
@@ -60,15 +40,6 @@ export const schema = {
                 {
                     "type": "model",
                     "properties": {}
-                },
-                {
-                    "type": "key",
-                    "properties": {
-                        "name": "byVideos",
-                        "fields": [
-                            "videosID"
-                        ]
-                    }
                 },
                 {
                     "type": "auth",
@@ -119,20 +90,6 @@ export const schema = {
                     "isRequired": false,
                     "attributes": []
                 },
-                "Notes": {
-                    "name": "Notes",
-                    "isArray": true,
-                    "type": {
-                        "model": "Note"
-                    },
-                    "isRequired": false,
-                    "attributes": [],
-                    "isArrayNullable": true,
-                    "association": {
-                        "connectionType": "HAS_MANY",
-                        "associatedWith": "videosID"
-                    }
-                },
                 "createdAt": {
                     "name": "createdAt",
                     "isArray": false,
@@ -178,5 +135,5 @@ export const schema = {
     },
     "enums": {},
     "nonModels": {},
-    "version": "bd64d03bb90025927fe90b29fcf761d0"
+    "version": "eaac571ab5553306aab1820094827bca"
 };
